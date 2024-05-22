08ddb3fc60f0f45738331a5a736e4a54e07eac20
Howard <howard@brightpage.tech>
2024-05-22 11:34:46 +0100

    Added individual pokemon page check

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('charmander page can be opened', function() {
+    cy.visit('http://localhost:5001/pokemon/ivysaur')
+    cy.get('[to="/pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
+  })
 })
