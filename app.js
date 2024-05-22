6ba49ed6b97e308a7b49ef47190170898a2281c3
Howard <howard@brightpage.tech>
2024-05-22 21:10:01 +0100

    #skip v12

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('11')
+  res.send('12')
 })
 
 app.get('/health', (req, res) => {
