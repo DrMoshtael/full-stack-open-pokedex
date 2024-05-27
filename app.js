2d2e025d5dc2d3c8ffc04bfafb01c6183670bf9d
Howard <howard@brightpage.tech>
2024-05-27 21:11:35 +0100

    Added Discord notification v13

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('12')
+  res.send('13')
 })
 
 app.get('/health', (req, res) => {
